65372433b7756c68e44959d179c19d7387e56c50
Ashish Mishra <ashishgk1999@gmail.com>
2024-01-18 16:23:35 +0530

    layout+header components

---
+++ frontend/src/Layout/Layout.tsx
@@ -0,0 +1,8 @@
+import Header from "../components/Header"
+
+const Layout=()=>{
+    return (
+        <div className="flex min-h-screen flex-col"><Header/></div>
+    )
+}
+export default Layout
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+const Header=()=>{
+    return (
+        <div className="bg-black py-6">
+            <div className="container mx-auto flex justify-between">
+                <span className="text-3xl text-white font-bold tracking-tight"> 
+                <Link to="/">MernHolidays.com</Link>
+                </span>
+                <span className='flex space-x-2'>
+                    <Link to="/sign-in" className='flex border border-black bg-red-700 text-black font-bold items-center hover:text-red-500 hover:bg-black hover:border-red-600 rounded-lg px-3'> Sign In</Link>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Header
